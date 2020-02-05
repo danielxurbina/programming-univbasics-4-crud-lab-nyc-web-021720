96b91a761fad2fe9b7258485453704bb8425babd
Daniel Urbina <danielur1999@gmail.com>
2020-02-05 02:59:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,11 +13,12 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
+  
 end
 
 def remove_element_from_start_of_array(array)
